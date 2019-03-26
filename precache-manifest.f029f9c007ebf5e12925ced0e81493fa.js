233469692a23c93b61dc906adf467b94dfe15e4d
Damian Krawczyk <damiankrawczyk23@gmail.com>
2019-03-26 10:17:27 +0000

    Updates

---
+++ precache-manifest.f029f9c007ebf5e12925ced0e81493fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12c1cece9b3e3372626a",
+    "url": "/To-do-list-react/static/js/runtime~main.12c1cece.js"
+  },
+  {
+    "revision": "664f96e83fe11750e3ba",
+    "url": "/To-do-list-react/static/js/main.664f96e8.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/To-do-list-react/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "664f96e83fe11750e3ba",
+    "url": "/To-do-list-react/static/css/main.6c881ae2.chunk.css"
+  },
+  {
+    "revision": "db325c4c90d8cbadbe3c069591227330",
+    "url": "/To-do-list-react/index.html"
+  }
+];
